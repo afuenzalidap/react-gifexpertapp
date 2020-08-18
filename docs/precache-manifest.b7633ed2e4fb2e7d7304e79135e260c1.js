65d4164161b941bfc9bebcbf938a45dc249983d6
Alexis Fuenzalida <afuenzalida@chilfresh.cl>
2020-08-18 13:08:06 -0400

    docs agregado

---
+++ docs/precache-manifest.b7633ed2e4fb2e7d7304e79135e260c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804796f95f72dcb7ff57029d7e64df8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34194175ff78014c89c7",
+    "url": "/static/css/main.4610dbd5.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34194175ff78014c89c7",
+    "url": "/static/js/main.d007d5f6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
